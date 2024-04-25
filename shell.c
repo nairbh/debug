59dd2845e4aaa57a2a17fe94dc184ff94039a4ad
nairbh <houcinenair@proton.me>
2024-04-25 20:22:54 +0200

    test

--- shell.c
+++ shell.c
@@ -18,45 +18,37 @@ int main(void)
 {
     char command[COMMAND_SIZE];
     char **args;
-    char **env = NULL;
     int is_interactive = isatty(STDIN_FILENO);
 
     signal(SIGINT, sigint_handler);
 
-    if (is_interactive) {
-        display_prompt(); 
-    }
-
     while (1)
     {
-        if (read_command(command, sizeof(command)) == 0) { 
-            continue;
-        }
-
-        if (strcmp(command, "exit") == 0)
-        {
-            break;
-        }
-
-        if (strcmp(command, "env") == 0)
-        {
-            for(env = environ; *env != NULL; env++)
-            {
-                printf("%s\n", *env);
-            }
-            continue;  
-        }
-
-        args = parse_args(command);
-        if (args[0] != NULL)
-        {
-            execute_command(args);
-        }
-        free(args);
-
         if (is_interactive) {
             display_prompt();
         }
+
+        if (!read_command(command, sizeof(command))) {
+            continue;  
+        }
+
+        if (strcmp(command, "exit") == 0) {
+            break; 
+        }
+
+        if (strcmp(command, "env") == 0) {
+            char **env;
+            for(env = environ; *env != NULL; env++) {
+                printf("%s\n", *env);
+            }
+            continue; 
+        }
+
+        args = parse_args(command);
+        if (args && args[0]) {
+            execute_command(args);
+        }
+        free(args);
     }
 
     return EXIT_SUCCESS;
@@ -69,39 +61,32 @@ void display_prompt(void)
 
 int read_command(char *command, size_t size)
 {
-    if (fgets(command, size, stdin) == NULL)
-    {
-        if (feof(stdin))
-        {
+    if (fgets(command, size, stdin) == NULL) {
+        if (feof(stdin)) { 
             printf("\n");
             exit(EXIT_SUCCESS);
-        }
-        else
-        {
+        } else {
             perror("Error reading input");
             exit(EXIT_FAILURE);
         }
     }
 
-    command[strcspn(command, "\n")] = '\0';
+    command[strcspn(command, "\n")] = '\0'; 
     return 1;
 }
 
 char **parse_args(char *command)
 {
     int argc = 0;
+    char *token = NULL;
     char **argv = malloc(sizeof(char *) * (COMMAND_SIZE + 1));
-    char *token;
-
-    if (argv == NULL)
-    {
+    if (argv == NULL) {
         perror("Failed to allocate argv");
         exit(EXIT_FAILURE);
     }
 
     token = strtok(command, " \t");
-    while (token != NULL && argc < COMMAND_SIZE)
-    {
+    while (token != NULL && argc < COMMAND_SIZE) {
         argv[argc++] = token;
         token = strtok(NULL, " \t");
     }
@@ -112,25 +97,17 @@ char **parse_args(char *command)
 
 void execute_command(char **args)
 {
-    pid_t pid;
-    int status;
-
-    pid = fork();
-    if (pid == -1)
-    {
+    pid_t pid = fork();
+    if (pid == -1) {
         perror("fork failed");
         exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("failed to execute");
+    } else if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("execution failed");
             exit(EXIT_FAILURE);
         }
-    }
-    else
-    {
+    } else {
+        int status;
         waitpid(pid, &status, 0);
     }
 }
@@ -139,5 +116,5 @@ void sigint_handler(int unused)
 {
     (void)unused;
     printf("\n");
-    fflush(stdout);
+    fflush(stdout); 
 }
