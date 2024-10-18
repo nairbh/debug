212bc046487b2c9fa7eae34b81ea4755dc16bad7
root <root@sunny>
2024-10-18 11:35:14 +0200

    update

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,69 +1,63 @@
 #!/usr/bin/python3
-"""
-    Module that set up basic HTTP Server
-"""
 
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.server
+from http.server import BaseHTTPRequestHandler
+import socketserver
+import requests
 import json
 
 
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    """
-    Class that create a simple server
-    """
+
 
     def do_GET(self):
-        """
-        Method that handle GET request"""
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!')
-        elif self.path == '/data':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            data = {
-                "name": "John",
-                "age": 30,
-                "city": "New York"
-            }
-            self.wfile.write(json.dumps(data).encode("utf-8"))
-        elif self.path == '/info':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            data = {
-                "version": "1.0",
-                "description": "A simple API built with http.server"
-            }
-            self.wfile.write(json.dumps(data).encode("utf-8"))
-        elif self.path == "/status":
+
+        if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"OK")
-        else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b'404 Not Found')
+
+            data = {
+                    "name": "John",
+                    "age": 30,
+                    "city": "New York"
+                    }
+
+            self.wfile.write(json.dumps(data).encode("utf-8"))
 
 
-def run(
-        server_class=HTTPServer,
-        handler_class=SimpleHTTPRequestHandler,
-        port=8000):
-    """ Function that run the server """
-    server_address = ('localhost', port)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            data = {
+                    "version": "1.0",
+                    "description": "A simple API buit with http.server"
+                    }
+
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_error(404, "Not Found")
 
 
-if __name__ == "__main__":
-    """
-        Main function that run the server
-    """
-    run()
+PORT = 8000
+Handler = SimpleHTTPRequestHandler
+ 
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+     print("serving at port", PORT)
+     httpd.serve_forever()
